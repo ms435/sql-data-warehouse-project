e5865d81a03d5244dd8797d2512483667ba0ff9d
Minh Chau Nguyen <143251364+ms435@users.noreply.github.com>
2025-04-24 01:39:10 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,11 @@
+/*
+=================================================
+Create Database and Schemas
+=================================================
+Script Purpose:
+This script creates a new database named 'DataWarehouse'
+*/
+
 CREATE database DataWarehouse;
 
 USE DataWarehouse;
